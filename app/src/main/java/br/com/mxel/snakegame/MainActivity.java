fb52c245c9f73daeeb85bb06a88a6434af2c31c7
Michel Ribeiro <michelrbr@gmail.com>
2018-05-17 14:48:32 -0300

    Create Project

---
+++ app/src/main/java/br/com/mxel/snakegame/MainActivity.java
@@ -0,0 +1,12 @@
+package br.com.mxel.snakegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
